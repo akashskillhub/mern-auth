c7c2eebd83d5d168002542476775bb1ad2629f77
akash <akash@gmail.com>
2023-05-29 10:17:36 +0530

    29-05-2023

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express")
 require("dotenv").config({ path: "./.env" })
 const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGO_URL)
+const cors = require("cors")
 const app = express()
-// Body Parser
 app.use(express.json())
+app.use(cors({
+    // origin: "https://auth-y8ff.onrender.com"
+    origin: "http://localhost:5173"
+}))
 
 app.use("/api/user", require("./routes/userRoutes"))
 
